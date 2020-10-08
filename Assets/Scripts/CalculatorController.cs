ca5ee519609a4efac18fc0d74366e256e2892da3
JChunX <jasonxie@live.unc.edu>
2020-10-08 00:26:17 -0500

    init commit

---
+++ Assets/Scripts/CalculatorController.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class CalculatorController : MonoBehaviour
+{
+    [SerializeField] private CalculatorDisplay _display;    // Encapsulate event generating class
+    private Stack<string> _opStack;
+    private Stack<int> _outStack;
+
+    //public delegate void DisplayEventHandler(string output);
+    //public event DisplayEventHandler SignaledOutput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _display.ReceivedOperand += HandleOperand;   // Add HandleOperand as listener to ReceivedOperand event
+        _display.ReceivedOperator += HandleOperator; // Similar
+        _display.ReceivedReset += Reset;
+        _opStack = new Stack<string>();
+        _outStack = new Stack<int>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Reset()
+    {
+        _opStack = new Stack<string>();
+        _outStack = new Stack<int>();
+    }
+
+    void OnDestory()
+    {
+        _display.ReceivedOperand -= HandleOperand;  // Remove listener on destroy
+        _display.ReceivedOperator -= HandleOperator;
+    }
+
+    void HandleOperand(string input)    // Operand event handler
+    {
+        //Debug.LogFormat("Operand: {0}", input);
+        _outStack.Push(Int32.Parse(input));
+    }
+
+    void HandleOperator(string input)
+    {
+        //Debug.LogFormat("Operator: {0}", input);
+
+        if (input.Equals("("))
+        {
+            _opStack.Push(input);
+            return;
+        }
+
+        if (input.Equals("="))
+        {
+            while (_opStack.Count != 0)
+            {
+                ToOut(_opStack.Pop());
+            }
+            return;
+        }
+
+        _display.Out("0");
+
+        if (input.Equals(")") && _opStack.Contains("("))
+        {
+            while (!(_opStack.Peek().Equals("(")))
+            {
+                ToOut(_opStack.Pop());
+            }
+            _opStack.Pop(); // Pop left parenthesis
+        }
+
+        if (input.Equals("*") || input.Equals("/"))
+        {
+            while(_opStack.Count != 0 
+                && !(_opStack.Peek().Equals("+") || _opStack.Peek().Equals("-") || _opStack.Peek().Equals("(")))
+            {
+                ToOut(_opStack.Pop());
+            }
+            _opStack.Push(input);
+        }
+
+        if (input.Equals("+") || input.Equals("-"))
+        {
+            while(_opStack.Count != 0 && !(_opStack.Peek().Equals("(")))
+            {
+                ToOut(_opStack.Pop());
+            }
+            _opStack.Push(input);
+        }
+
+        string operands = "";
+        foreach(int operand in _outStack)
+        {
+            operands += operand.ToString();
+            operands += ", ";
+        }
+        Debug.Log("----------------------------------------------------------------");
+        Debug.Log(operands);
+
+        string ops = "";
+        foreach (string op in _opStack)
+        {
+            ops += op;
+            ops += ", ";
+        }
+        Debug.Log(ops);
+    }
+
+    void ToOut(string op)
+    {
+        int x = _outStack.Pop();
+        int y = _outStack.Pop();
+        int z = 0;
+
+        switch (op)
+        {
+            case "+":
+                z = y + x;
+                break;
+            case "-":
+                z = y - x;
+                break;
+            case "*":
+                z = y * x;
+                break;
+            case "/":
+                z = y / x;
+                break;
+        }
+        //Debug.LogFormat("z: {0}", z);
+        _display.Out(z.ToString());
+        _outStack.Push(z);
+    }
+}
---
+++ Assets/Scripts/CalculatorDisplay.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CalculatorDisplay : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _TextField;
+
+    public delegate void OperandEventHandler(string input); // Delegate is a method prototype for handling events
+    public event OperandEventHandler ReceivedOperand; // The event
+
+    public delegate void OperatorEventHandler(string input);
+    public event OperatorEventHandler ReceivedOperator;
+
+    public delegate void ResetEventHandler();
+    public event ResetEventHandler ReceivedReset;
+
+    private string _curOperand;
+    private string _prevInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _TextField.SetText("0");
+        _curOperand = "0";
+        _prevInput = "start";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Put(string input) 
+    {
+        switch (input)
+        {
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+            case "0":
+                if (input == "0" && _curOperand == "0")
+                {
+                    break;
+                }
+                else if (input != "0" && _curOperand == "0")
+                {
+                    _curOperand = input;
+                }
+                else if (_curOperand.Length < 8)
+                {
+                    _curOperand = _curOperand + input;
+                }
+
+                _TextField.SetText(_curOperand);
+
+                if (input == "0")
+                {
+                    _prevInput = input;
+                }
+                else
+                {
+                    _prevInput = "num";
+                }
+                break;
+            case "+":
+            case "-":
+            case "*":
+            case "/":
+            case "=":
+                if (_prevInput != "+" && _prevInput != "-" && _prevInput != "*" && _prevInput != "/" && _prevInput != "=")
+                {
+                    if (_prevInput == "num" || _prevInput == "0" || _prevInput == "start") // check if operating on user entered number, else do not push current operand (already on outstack)
+                    {
+                        ReceivedOperand?.Invoke(_curOperand);   // Event usage: invoke event handler
+                    }
+                    ReceivedOperator?.Invoke(input);
+                    _curOperand = "0";
+                    _prevInput = input;
+                }
+
+                else if (_prevInput.Equals("="))
+                {
+                    ReceivedOperator?.Invoke(input);
+                    _prevInput = input;
+                }
+                break;
+            case "(":
+                if (_prevInput == "num" || _prevInput == "0")
+                {
+                    ReceivedOperand?.Invoke(_curOperand);
+                    ReceivedOperator?.Invoke(input);
+                    _prevInput = input;
+                    break;
+                }
+                if (_prevInput != "+" || _prevInput != "-" || _prevInput != "*" || _prevInput != "/" || _prevInput == "start")
+                {
+                    ReceivedOperator?.Invoke(input);
+                    _curOperand = "0";
+                    _prevInput = input;
+                }
+                break;
+            case ")":
+                if (_prevInput == "num")
+                {
+                    ReceivedOperand?.Invoke(_curOperand);
+                    ReceivedOperator?.Invoke(input);
+                    _prevInput = input;
+                    break;
+                }
+                if (_prevInput == ")")
+                {
+                    ReceivedOperator?.Invoke(input);
+                    _prevInput = input;
+                }
+                break;
+            case "+/-": // not implemented
+                break;
+            case "c":
+                if (_prevInput == "num")
+                {
+                    Start();
+                }
+                break;
+            case "ac":
+                ReceivedReset?.Invoke();
+                Start();
+                break;
+        }
+    }
+
+    public void Out(string output)
+    {
+        _curOperand = output;
+        _TextField.SetText(output);
+    }
+
+    public void Clear()
+    {
+        _TextField.SetText("0");
+    }
+
+}
